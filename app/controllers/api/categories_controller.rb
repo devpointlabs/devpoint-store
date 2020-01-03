c1f8fd5c8132673d610fe0d30e7453b85dd29913
David Mendez <mendezdavid68@gmail.com>
2020-01-02 22:00:12 -0700

    CategoriesController built

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,24 +1,38 @@
 class Api::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :destroy]
 
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
   end
-
-  def update
-  end
+end
 
   def destroy
+    @category.destroy
+    redirect_to category_path
   end
 
   private
    def set_category
+    @category = Category.find(params[:id])
    end
 
    def category_params
+    params.require(:user).permit(:name)
    end
 end
