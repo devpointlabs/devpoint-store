9125d7714c63d5798c26403ec37d37eca446b2ac
Clarke Cirel <Clarke.cirel@gmail.com>
2020-01-03 16:21:16 -0700

    ItemView page added, App.js updated with route

---
+++ client/src/components/ItemView.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class ItemView extends React.Component {
+  state = { }
+
+  componentDidMount() {
+    axios.get('/api/items/${id  }')
+    .then( res => {
+      this.setState
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Item View Page
+      </div>
+    )
+  }
+}
+
+export default ItemView
