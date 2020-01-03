e02de5ed588a253c975fcd12a0a42682370e9597
Faith <faithhafen@gmail.com>
2020-01-02 20:56:43 -0700

    added routes, added home, nomatch, navbar, login, and register components

---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Header } from 'semantic-ui-react'
+
+const NoMatch = () => (
+  <Header as='h3' textAlign='center'>
+    Page not found
+    Go back to <Link to='/'> Shop </Link>
+  </Header>
+)
+
+export default NoMatch
