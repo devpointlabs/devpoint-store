8e1fcb173a385e7a3096537ba2519353b27d47be
su8lim3kid <aldereteangel21@gmail.com>
2020-01-03 16:32:48 -0700

    cards for products on home page

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "semantic-ui-css/semantic.min.css"
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './providers/AuthProvider'
 import { initMiddleware } from 'devise-axios'
+
 
 initMiddleware()
 
