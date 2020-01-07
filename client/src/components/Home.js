1efa6c5e644d82032dbbec010ae1de6db188a16f
su8lim3kid <aldereteangel21@gmail.com>
2020-01-07 10:35:25 -0700

    Home Page 2.0

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,256 +1,84 @@
 import React from "react"
-import {  Card, Image, Header } from 'semantic-ui-react'
+import {  Grid, Image } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
-import shirt from './Images/shirt.png'
-import hat from './Images/hat.png'
-import mug from './Images/mug.png'
+
+import tshirt from './Images/tshirt.png'
 import hoodie from './Images/hoodie.png'
-import styled from 'styled-components'
+import hats from './Images/hats.png'
+import stickers from './Images/stickers.png'
 
 
- class Home extends React.Component {
+
+
+class Home extends React.Component {
   render() {
     return (
-      <Page>
-
-      <Header style={{
-                fontSize: "75px",
-                fontFamily: "Luminari, fantasy",
-                textShadow: "2px 3px #3299a8",
-                height: '80px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                position:'absolute, center',
-                color: '#e0d1b8',
-              }}>
-
-      Swag
-
-        
-
-        </Header>
-
-      <br/>
-      <br/>
-      <br/>
-      <>
-      <Card.Group itemsPerRow="5">
-        <Link to={`/ItemView`}>
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={shirt} alt="Shirt" />
-
-          
-          <Card.Content>
-              
-          T-Shirt
-
-          </Card.Content>
-          <Card.Content extra>
-            $25
-          </Card.Content>
-
-        </Card>
-        </Link>
-
-        <Link to={`/ItemView`}>
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={hat} alt="Hat" />
-          <Card.Content>
-            Hat
-
-          </Card.Content>
-          <Card.Content extra>
-            $25
-          </Card.Content>
-
-        </Card>
-        </Link>
-
-        <Link to={`/ItemView`}>
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={mug} alt="Mug" />
-          <Card.Content>
-          Mug
-
-          </Card.Content>
-          <Card.Content extra>
-            $15
-          </Card.Content>
-
-        </Card>
-        </Link>
-
-
-        <Link to={`/ItemView`}>
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={hoodie} alt="Hoodie" />
-          <Card.Content>
-            Hoodie
-
-          </Card.Content>
-          <Card.Content extra>
-            $50
-          </Card.Content>
-
-        </Card>
-        </Link>
-        <Link to={`/ItemView`}>
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={shirt} alt="Shirt" />
-          <Card.Content>
-          T-Shirt
-
-          </Card.Content>
-          <Card.Content extra>
-            $25
-          </Card.Content>
-
-        </Card>
-        </Link>
-
-        <Link to={`/ItemView`}>
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={hat} alt="Hat" />
-          <Card.Content>
-            Hat
-
-          </Card.Content>
-          <Card.Content extra>
-            $25
-          </Card.Content>
-
-        </Card>
-        </Link>
-
-        <Link to={`/ItemView`}>
-
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={mug} alt="Mug" />
-          <Card.Content>
-          Mug
-
-          </Card.Content>
-          <Card.Content extra>
-            $15
-          </Card.Content>
-
-        </Card>
-        </Link>
-
-        <Link to={`/ItemView`}>
-
-        <Card style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{
-                  height: '320px',
-                  width: '300px',
-                }}
-              
-            Image src={hoodie} alt="Hoodie" />
-          <Card.Content>
-            Hoodie
-
-          </Card.Content>
-          <Card.Content extra>
-            $50
-          </Card.Content>
-
-        </Card>
-        </Link>
-      </Card.Group>
-
-      
-
-      </>
-      </Page>
-    )
+    
+      <Grid centered columns={1}>
+      <Grid.Column  >
+    <Link to={`ItemView`}>
+        <Image src={tshirt}
+        style={{
+          height: '350px',
+          width: '1200px',
+          boxShadow: '10px 10px 5px grey',
+          display: 'block',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+        />
+    </Link>
+      </Grid.Column>
+    
+       <Grid.Row centered columns={3}>
+          <Grid.Column>
+        <Link to={`ItemView`}>
+            <Image src={hats}
+              style={{
+               height: '400px',
+               width: '500px',
+                boxShadow: '10px 10px 5px grey',
+               }} />
+          </Link>
+           </Grid.Column>
+        <Grid.Column>
+        <Link to={`ItemView`}>
+          <Image src={hoodie}
+            style={{
+              height: '400px',
+              width: '500px',
+              boxShadow: '10px 10px 5px grey',
+            }} />
+            </Link>
+        </Grid.Column>
+      </Grid.Row>
+  
+      <Grid.Row centered columns={3}>
+        <Grid.Column>
+        <Link to={`ItemView`}>
+          <Image src={stickers}
+          style={{
+            height: '400px',
+            width: '400vw',
+            boxShadow: '10px 10px 5px grey',
+          }}  />
+          </Link>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+)
+    
   }
 }
+       
+       
 
-const Page = styled.div`
-  /* padding-left:120px; */
-  padding-left: 140px;
-  padding-right: 90px;
-`
 
+ 
 
 
 
 
-export default Home;
+export default Home
+
 
