f88d060147f7ebc588c0ad23c1ffef723d55f115
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2020-01-06 17:28:49 -0700

    added footer v1

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import NoMatch from './components/NoMatch'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
 import { Switch, Route } from 'react-router-dom'
+import Footer from './components/Footer'
 
 const App = () => (
   <>
@@ -24,6 +25,8 @@ const App = () => (
       <Route component={NoMatch} />
     </Switch>
   </FetchUser>
+  <br />
+  <Footer />
   </>
 )
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return(
+
+      <div class="ui inverted black vertical footer segment" style={{ 
+        fontSize: "75px",
+        fontFamily: "Luminari, fantasy",
+        height: '80px',
+        display: 'flex',
+        }} >
+      <div class="ui center aligned container">
+      <h4 class="ui inverted header"> 2020 DevPoint Labs / 370 S. 300 E. SLC, Utah 84111c / 801-448-7240 / contact@devpointlabs.com </h4>
+      </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+export default Footer
