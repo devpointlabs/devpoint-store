d1f3f3d96891fd08de9a0c0fdd825c3b8718d646
su8lim3kid <aldereteangel21@gmail.com>
2020-01-08 16:55:35 -0700

    Home 3.0

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react"
 import axios from "axios"
-import {  Grid, Image } from 'semantic-ui-react'
+import {  Container, Card, Image } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
 import tshirt from './Images/tshirt.png'
 import hoodie from './Images/hoodie.png'
 import hats from './Images/hats.png'
 import stickers from './Images/stickers.png'
+import '../styles/home.css'
 
 
 class Home extends React.Component {
@@ -21,67 +22,53 @@ class Home extends React.Component {
     })
   }
 
+  renderPrimaryCategory() {
+    return this.state.categories.map(c => (
+      <div>
+        {c.name}
+      </div>
+    ))
+  }
+  
+  renderCategories() {
+    return this.state.categories.map(c => (
+      <>
+      <Card.Group itemsPerRow={1}>
+      <Card>
+        <Card.Content>
+          <Link to={`/categories/c.id`}  >
+            <Image src={c.image} id='shirtpic'/>
+          </Link>
+        </Card.Content>
+      </Card>
+    </Card.Group>
+    
+    {/* <Card.Group itemsPerRow={2}>
+      <Card>
+        <Card.Content>
+          <Link to={`/categories/c.id`}  >
+            <Image src={hoodie} id='hoodiepic'/>
+          </Link>
+        </Card.Content>
+      </Card>
+
+    </Card.Group> */}
+    </>
+    ))
+  }
+
   render() {
     return (
-    
-      <Grid centered columns={1}>
-      <Grid.Column  >
-    <Link to={`/categories/c.id`}  >
-        <Image src={tshirt}
-        style={{
-          height: '500px',
-          width: '1200px',
-          boxShadow: '10px 10px 5px grey',
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-        />
-    </Link>
-      </Grid.Column>
-    
-       <Grid.Row centered columns={3}>
-          <Grid.Column>
-        <Link to={`/categories/2`}>
-            <Image src={hats}
-              style={{
-               height: '500px',
-               width: '600px',
-                boxShadow: '10px 10px 5px grey',
-               }} />
-          </Link>
-           </Grid.Column>
-        <Grid.Column>
-        <Link to={`/categories/3`}>
-          <Image src={hoodie}
-            style={{
-              height: '500px',
-              width: '600px',
-              boxShadow: '10px 10px 5px grey',
-            }} />
-            </Link>
-        </Grid.Column>
-      </Grid.Row>
-  
-      <Grid.Row centered columns={3}>
-        <Grid.Column>
-        <Link to={`/categories/4`}>
-          <Image src={stickers}
-          style={{
-            height: '500px',
-            width: '600px',
-            boxShadow: '10px 10px 5px grey',
-          }}  />
-          </Link>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-)
-    
-  }
-}
-       
-      
+      <Container>
+
+
+          {this.renderCategories()}
+
+
+        </Container>
+
+)}}
+
 export default Home
 
 
