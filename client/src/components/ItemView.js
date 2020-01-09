34d96179855152a9f38150358e73bf7ded953df0
Faith <faithhafen@gmail.com>
2020-01-09 10:59:00 -0700

    pull

--- client/src/components/ItemView.js
+++ client/src/components/ItemView.js
@@ -5,9 +5,10 @@ import gshirtB from './Images/gshirt-b.jpg'
 import gshirtW from './Images/gshirt-w.jpg'
 import gshirtWB from './Images/gshirt-wb.jpg'
 import styled from 'styled-components'
-import { Container, Button, Image, Header, Dropdown, Form, TextArea } from 'semantic-ui-react'
+import { Container, Button, Image, Header, Dropdown, Form } from 'semantic-ui-react'
 
 // TODO: make responsive
+// TODO: modify + add code to be responsive to data when db is made
 
 // below are size options to make demo work, will need to change for real data later
 
@@ -44,8 +45,8 @@ class ItemView extends React.Component {
 }
 
   render() {
-    // const { match: { params: { id, category_id } } } = this.props
-    // const { name, description, price } = this.state.item
+    const { match: { params: { id, category_id } } } = this.props
+    const { name, desc, price, image } = this.state.item
 
   return(
     <>
@@ -55,25 +56,27 @@ class ItemView extends React.Component {
           {/* will need to adjust function for item.image */}
         {(() => {
         switch (this.state.currentImage) {
-          case 1:   return <Image src={gshirtB} />;
-          case 2: return <Image src={gshirtW} />;
-          case 3:  return <Image src={gshirtWB} />;
-          default:      return <Image src={gshirt} />;
+          case 1: return <Image src={image} />
+          case 2: return <Image src={gshirtW} />
+          case 3: return <Image src={gshirtWB} />
+          default: return <Image src={gshirt} />
         }
       })()}
 
           <Mini style={{ }}>
-          <div center> <Image src={gshirt} onClick={ () =>  this.setState({ currentImage: 0 }) } /> </div> {/* placeholder for db images - clarke suggested a carousel? */}
+          <div> <Image src={gshirt} onClick={ () =>  this.setState({ currentImage: 0 }) } /> </div> {/* placeholder for db images - clarke suggested a carousel? */}
           <div> <Image src={gshirtB} onClick={ () => this.setState({ currentImage: 1 }) }/> </div>
           <div> <Image src={gshirtW} onClick={ () =>  this.setState({ currentImage: 2 }) }/> </div>
           <div> <Image src={gshirtWB} onClick={ () => this.setState({ currentImage: 3 }) }/> </div>
           </Mini>
         </div>
         
+        {/* possiblity to make this into second Item/Cart Form and render here instead */}
+
         <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.03)', height: '550px', width: '450px', padding: '40px'}}>
-          <Header as='h1'> Neat Shirt </Header> {/* item.name */}
-          <Header as='h2' style={{ color: '#A9A9A9' }}> $25.00 </Header> {/* item.price */}
-          <Header as='h3'> Size </Header> {/* item.size */}
+          <Header as='h1'> { name } </Header>
+          <Header as='h2' style={{ color: '#A9A9A9' }}> $ { price }.00 </Header>
+          <Header as='h3'> Size </Header>
             <Dropdown
             placeholder='Select Size'
             selection
@@ -82,16 +85,15 @@ class ItemView extends React.Component {
             />
           <Header as='h3'> Quantity </Header> {/* item.quantity ? */}
             <Form>
-              <TextArea style={{ height: '45px', width: '120px', margin: '0px 0px 20px 0px', backgroundColor: '#ececec' }} placeholder='1' />
+              {/* will need to write an onchange function here */}
+              <Form.TextArea defaultValue={1} style={{ height: '45px', width: '120px', margin: '0px 0px 20px 0px', backgroundColor: '#ececec' }} placeholder='1' />
             </Form>
           <Button size='huge' basic color='black' style={{ margin: '20px 0px 0px 0px'}}> Add to Cart </Button>
         </div>
       </Grid>
 
       <Desc>
-        {/* <p> { item.desc } </p> */}
-        <p> When a black cat crosses your path, nothing happens. It's just a cat.</p>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat enim vitae lectus consectetur laoreet. Maecenas in massa vel leo efficitur mattis sed eget massa. Pellentesque posuere, nibh non tempus egestas, erat nisi interdum elit, vitae eleifend elit nunc eget massa. Proin et urna ut risus gravida euismod non vitae nisl. </p>
+        <p> { desc } </p>
       </Desc>
     </Container>
     </>
