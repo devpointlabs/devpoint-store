e93b66e8d4958ece90aeb063f39bc8528c54fd46
su8lim3kid <aldereteangel21@gmail.com>
2020-01-14 20:55:29 -0700

    sort button and croped images on category view page

--- client/src/components/CategoryView.js
+++ client/src/components/CategoryView.js
@@ -2,12 +2,13 @@ import React from "react";
 import axios from "axios";
 import ItemForm from "./ItemForm";
 import { Link } from "react-router-dom";
-import { Image, Card, Container, Button, Grid, Segment } from "semantic-ui-react";
+import { Image, Card, Container, Button, Grid, Segment,Dropdown } from "semantic-ui-react";
 import '../styles/catView.css'
 
 
 class CategoryView extends React.Component {
   state = { category: {}, items: [] };
+  
 
   // Mount used to make API calls to update state
   componentDidMount() {
@@ -70,7 +71,23 @@ class CategoryView extends React.Component {
     ));
   }
 
+
   render() {
+    // const options = [
+    //   { key: 1, text: 'Price: Low to High', value: 1 },
+    //   { key: 2, text: 'Price: High to Low', value: 2 },
+    //   { key: 3, text: 'Best Selling', value: 3 },
+    //   { key: 4, text: 'On sale', value: 4 },
+      
+
+    // ]
+
+    const options = [
+      { key: 1, text: 'Price: Low to High', value: 1 },
+      { key: 2, text: 'Price: High to Low', value: 2 },
+      { key: 3, text: 'Most Popular', value: 3 },
+      { key: 4, text: 'On Sale', value: 4 },
+    ]
     return (
       <>
         <Container>
@@ -80,10 +97,21 @@ class CategoryView extends React.Component {
               <Card.Content>
                 <Image id ='mainimage'src={this.state.category.image} />
                 <div id="shadowy"></div>
-                <h1 id='shirty'>{this.state.category.name}</h1>
+                <h1 id='cardname'>{this.state.category.name}</h1>
+                <h3 id='sorty'>Sort By:</h3>
+                <Button.Group
+                 id='sortbutton'
+                 inverted color='black'>
+                  <Dropdown
+                    className='button icon'
+                    floating
+                    options={options}
+                    placeholder='Select an Option' />
+                </Button.Group>
               </Card.Content>
             </Card>
           </Card.Group>
+   
         <Segment>
           <ItemForm category_id={this.props.match.params.id} add={this.add} />
         </Segment>
@@ -99,6 +127,10 @@ class CategoryView extends React.Component {
     );
   }
 }
+
+
+
+
 
 export default CategoryView;
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import axios from "axios"
-import {  Container, Card, Image } from 'semantic-ui-react'
+import {  Container, Card, Image, Grid } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
 import '../styles/home.css'
 
@@ -26,12 +26,12 @@ class Home extends React.Component {
     return(
     
     <Card id='card1'>
-    <Card.Content>
-      <Link to={`/categories/${c.id}`} >
-        <Image src={c.image} id='shirtpic'/>
-        <div id="shadow1"></div>
-        <h1 id='shirty'>T-Shirt</h1>
-      </Link>
+      <Card.Content>
+        <Link to={`/categories/${c.id}`} >
+         <Image src={c.image} id='shirtpic'/>
+           <div id="shadow1"></div>
+           <h1 id='shirty'>T-Shirt</h1>
+        </Link>
       </Card.Content>
     </Card>    
    
@@ -51,8 +51,8 @@ class Home extends React.Component {
        <Card.Content >                 
          <Link to={`/categories/${c.id}`} >
           <Image  src={c.image} id='restimages'/>
-           <div id="shadow"></div>
-           <h1 id='shirty'>{c.name}</h1>
+            <div id="shadow"></div>
+            <h1 id='shirty'>{c.name}</h1>
          </Link>
         </Card.Content>
       </Card>         
@@ -66,9 +66,12 @@ class Home extends React.Component {
         <Card.Group itemsPerRow={1}>  
           {this.renderImages()}
         </Card.Group>
-        <Card.Group itemsPerRow={2} >
+       
+        <div class="stacking cards">
+        <Card.Group itemsPerRow={2}  >
           {this.renderImagessmall()}
         </Card.Group>
+        </div>
       </Container>
 
 )}}
