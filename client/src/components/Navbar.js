fafdf640ea3fea77ab73271dc70aa551bdcff57f
David Mendez <mendezdavid68@gmail.com>
2020-01-14 17:12:25 -0700

    update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,18 +16,16 @@ const Navbar = (props) => {
   const [hats, setHats] = useState({})
   const [stickers, setStickers] = useState({})
 
-  // const options = categories.map( c => { return { T-shirts: c, Hats: c, Hoodies: c, }} );
-
   useEffect(() => {
     axios.get("/api/categories")
-    .then(res => {
-      setTshirts(res.data[0])
-      setHats(res.data[1])
-      setHoodies(res.data[2])
-      setStickers(res.data[3])
-    })
+      .then(res => {
+        setTshirts(res.data[0])
+        setHats(res.data[1])
+        setHoodies(res.data[2])
+        setStickers(res.data[3])
+      })
   }, []);
-  
+
   const menuOptions = [
     { key: 1, text: 'Hats', value: 'Hats' },
     { key: 2, text: 'Stickers', value: 'Stickers' },
@@ -40,32 +38,32 @@ const Navbar = (props) => {
   const handleChange = (e, { value, }, ) => setSelection(value)
 
   const userOptions = () => {
-      switch (selection) {
-        case "Hats":
-          props.history.push(`/categories/${hats.id}`)
-          setSelection('')
-          break;
+    switch (selection) {
+      case "Hats":
+        props.history.push(`/categories/${hats.id}`)
+        setSelection('')
+        break;
 
-        case "Stickers":
-          props.history.push(`/categories/${stickers.id}`)
-          setSelection('')
-          break;
-          
-        case 'Contact':
-          props.history.push('/Contact')
-          setSelection('')
-          break;
+      case "Stickers":
+        props.history.push(`/categories/${stickers.id}`)
+        setSelection('')
+        break;
 
-        case "Register":
-          props.history.push('/Register')
-          setSelection('')
-          break;
+      case 'Contact':
+        props.history.push('/Contact')
+        setSelection('')
+        break;
 
-        case "Login":
-          props.history.push('/login')
-          setSelection('')
-          break;
-      }
+      case "Register":
+        props.history.push('/Register')
+        setSelection('')
+        break;
+
+      case "Login":
+        props.history.push('/login')
+        setSelection('')
+        break;
+    }
   }
 
   // const rightNavItems = (auth) => {
@@ -114,95 +112,95 @@ const Navbar = (props) => {
   //       </>
   //     )
   //   }
-// }
+  // }
 
-return (
-  <AuthConsumer>
-    {auth => (
-      <div id='whole navbar'>
-        <div id='left side navbar'>
-          <div style={home}>
-            <NavLink
-              to="/" exact
-              activeClassName="active"
-              className="nav-link"
-            >
-              <div style={text}>
-                <img
-                  src={minilogo}
-                  height="60"
-                  width="60"
-                  class="navbar"
-                />
-                .SHOP( )
+  return (
+    <AuthConsumer>
+      {auth => (
+        <div id='whole navbar'>
+          <div id='left side navbar'>
+            <div style={home}>
+              <NavLink
+                to="/" exact
+                activeClassName="active"
+                className="nav-link"
+              >
+                <div style={text}>
+                  <img
+                    src={minilogo}
+                    height="60"
+                    width="60"
+                    class="navbar"
+                  />
+                  .SHOP( )
                 </div>
-            </NavLink>
+              </NavLink>
+            </div>
+          </div>
+
+          <div id='right side navbar' style={cust}>
+            <div>
+              <NavLink to='/' exact>
+                <h3 style={text}>
+                  All Products
+              </h3>
+              </NavLink>
+            </div>
+
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+
+            <div>
+              <NavLink to={`/categories/${tShirts.id}`}>
+                <h3 style={text}>
+                  T-Shirts
+              </h3>
+              </NavLink>
+            </div>
+
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+
+            <div>
+              <NavLink to={`/categories/${hoodies.id}`}>
+                <h3 style={text}>
+                  Hoodies
+              </h3>
+              </NavLink>
+            </div>
+
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+
+            {selection != '' ? userOptions() : null}
+
+            <div style={lift} >
+              <Dropdown
+                style={text}
+                text='More'
+                as="h3"
+                options={menuOptions}
+                onChange={handleChange}
+                value={selection}
+                simple item
+              />
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <div>
+              <Link
+                to="/Cart"
+                active={props.location.pathname === '/Cart'}
+              >
+                <h3 style={text}>
+                  <Icon
+                    name='cart arrow down'>
+                  </Icon>
+                  Cart
+              </h3>
+              </Link>
+            </div>
           </div>
         </div>
-
-        <div id='right side navbar' style={cust}>
-          <div>
-            <NavLink to='/' exact>
-              <h3 style={text}>
-                All Products
-              </h3>
-            </NavLink>
-          </div>
-
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-
-          <div>
-            <NavLink to={`/categories/${tShirts.id}`}>
-              <h3 style={text}>
-                T-Shirts
-              </h3>
-            </NavLink>
-          </div>
-
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-
-          <div>
-            <NavLink to={`/categories/${hoodies.id}`}> 
-              <h3 style={text}>
-                Hoodies
-              </h3>
-            </NavLink>
-          </div>
-
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-
-          {selection != '' ? userOptions() : null}
-
-          <div style={lift} >
-            <Dropdown
-              style={text}
-              text='More'
-              as="h3"
-              options={menuOptions}
-              onChange={handleChange}
-              value={selection}
-              simple item
-            />
-          </div>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <div>
-            <Link
-              to="/Cart"
-              active={props.location.pathname === '/Cart'}
-            >
-              <h3 style={text}>
-                <Icon
-                  name='cart arrow down'>
-                </Icon>
-                Cart
-              </h3>
-            </Link>
-          </div>
-        </div>
-      </div>
-    )}
-  </AuthConsumer>
-)
+      )}
+    </AuthConsumer>
+  )
 }
 
 const cust = {
