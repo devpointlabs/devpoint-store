d9259b19c03e1061fa47857c8a9f91a65b6d7da3
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2020-01-15 15:16:57 -0700

    single item, and size added to seeds file, also fixed footer spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,26 @@
     name: 'T-shirts',
     image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578519422/shirtimage_lsexnh.jpg',
     full_width: 'true'
-  )
+    )
 
-  16.times do
-    Item.create(
-      name: Faker::Lorem.words(number: 1),
+  1.times do
+    item = Item.create(
+      name: 'T Shirt',
       price: 25.00,
       desc: Faker::Lorem.paragraph(sentence_count: 3),
-      image: Faker::LoremFlickr.image(size: "150x180", search_terms: ['fashion']),
+      image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958317/shortsleeve2_fqroqm.jpg',
       category_id: cat.id
-    )
+      )
+
+    1.times do
+      ItemVariant.create(
+        size: 'small',
+        quantity: '3',
+        image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958317/shortsleeve2_fqroqm.jpg',
+        back_image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958309/shortsleevefront_mvg81i.jpg',
+        item_id: item.id
+        )
+    end
   end
 end
 
@@ -23,15 +33,25 @@ end
     full_width: 'false'
     )
 
-  16.times do
-    Item.create(
-      name: Faker::Lorem.words(number: 1),
+  1.times do
+    item = Item.create(
+      name: 'Hat',
       price: 15.00,
       desc: Faker::Lorem.paragraph(sentence_count: 3),
-      image: Faker::LoremFlickr.image(size: "150x180", search_terms: ['fashion']),
+      image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958403/hat_wwqd8t.png',
       category_id: cat.id
-    )
-  end
+      )
+
+    1.times do
+      ItemVariant.create(
+        size:'one size fits all',
+        quantity:'3',
+        image:'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958403/hat_wwqd8t.png',
+        back_image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958403/hat_wwqd8t.png',
+        item_id: item.id
+        )
+    end
+  end 
 end
 
 1.times do
@@ -39,16 +59,26 @@ end
     name: 'Hoodies',
     image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578519441/hoodieimage_uqkzat.jpg',
     full_width: 'false'
-  )
+    )
 
-  16.times do
-    Item.create(
-      name: Faker::Lorem.words(number: 1),
+  1.times do
+    item = Item.create(
+      name: 'Grey Hoodie',
       price: 35.00,
       desc: Faker::Lorem.paragraph(sentence_count: 3),
-      image: Faker::LoremFlickr.image(size: "150x180", search_terms: ['fashion']),
+      image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958630/hoodiefront2_o0qskk.jpg',
       category_id: cat.id
-    )
+      )
+
+    1.times do
+      ItemVariant.create(
+        size: 'small',  
+        quantity: '3',
+        image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958630/hoodiefront2_o0qskk.jpg',
+        back_image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958621/hoodieback_hevoyw.jpg',
+        item_id: item.id
+        )
+    end
   end
 end
 
@@ -57,15 +87,25 @@ end
     name: 'Stickers',
     image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578519447/stickerimage_o87upo.jpg',
     full_width: 'false'
-  )
+    )
 
-  16.times do
-    Item.create(
+  1.times do
+    item = Item.create(
       name: Faker::Lorem.words(number: 1),
       price: 5.00,
       desc: Faker::Lorem.paragraph(sentence_count: 3),
-      image: Faker::LoremFlickr.image(size: "150x180", search_terms: ['fashion']),
+      image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958417/sticker1_xhisdz.jpg',
       category_id: cat.id
-    )
+      )
+
+    1.times do
+      ItemVariant.create(
+        size: 'one size fits all',
+        quantity: '5',
+        image: 'https://res.cloudinary.com/dyhj8aqsh/image/upload/v1578958417/sticker1_xhisdz.jpg',
+        back_image: '',
+        item_id: item.id
+        )
+    end
   end
 end
