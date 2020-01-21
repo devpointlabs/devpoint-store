5594dbbf11701228d608c4a77fe5af4041f7c0b1
su8lim3kid <aldereteangel21@gmail.com>
2020-01-21 13:25:56 -0700

    contact form set up and working

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,55 +1,95 @@
-import React, { Component } from 'react'
-import { Button, Form, Input, TextArea } from 'semantic-ui-react'
+import React, { useState } from "react"
+import axios from "axios"
+import { Form } from "semantic-ui-react"
+import styled from "styled-components"
+import { StyledButton, StyledHeader } from "../styles/shared"
 
-class Contact extends Component {
-  state = {}
+const Contact = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [subject, setSubject] = useState("")
+  const [message, setMessage] = useState("")
 
-  handleChange = (e, { value }) => this.setState({ value })
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios
+      .get(
+        `/api/contact?name=${name}&email=${email}&subject=${subject}&message=${message}`
+      )
+      .then(res => {
+        console.log("email sent")
+        setName("")
+        setEmail("")
+        setSubject("")
+        setMessage("")
+      })
+  };
 
-  render() {
-    const { value } = this.state
-    return (
-      <>
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Field
-            control={Input}
-            label='First name'
-            placeholder='First name'
-          />
-          <Form.Field
-            control={Input}
-            label='Last name'
-            placeholder='Last name'
-          />
-          <Form.Field
-            control={Input}
-            label='email'
-            placeholder='email'
-          />
-          <Form.Field
-            control={Input}
-            label='Phone Number'
-            placeholder='Phone number'
-          />
-
-        </Form.Group>
-        
-        <Form.Field
-          control={TextArea}
-          label='Message'
-          placeholder='Tell us more about whats going on'
+  return (
+    <>
+      <StyledHeader as="h1">Contact Us</StyledHeader>
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <StyledInput
+          label="Name"
+          placeholder="Name"
+          type="text"
+          name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
-
-        <Form.Field control={Button}>Submit</Form.Field>
+        <StyledInput
+          label="Email"
+          placeholder="Email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <StyledInput
+          label="Subject"
+          placeholder="Subject"
+          type="text"
+          name="subject"
+          value={subject}
+          onChange={e => setSubject(e.target.value)}
+        />
+        <StyledTextArea
+          label="Message"
+          placeholder="Message"
+          type="text"
+          name="message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
+        <StyledButton type="submit">Submit</StyledButton>
       </Form>
-      <hr />
-      </>
-      
-    )
-    
+    </>
+  );
+};
+const StyledInput = styled(Form.Input)`
+  & > label {
+    color: white !important;
+    font-family: "Audiowide", cursive;
   }
-}
+  & > div {
+    & > input {
+      background: #1c1c1d !important;
+      color: white !important;
+    }
+  }
+`;
 
+const StyledTextArea = styled(Form.TextArea)`
+  & > label {
+    color: white !important;
+    font-family: "Audiowide", cursive;
+    & > textarea {
+      background: #1c1c1d !important;
+      height: 200px;
+      color: white !important;
+    }
+  }
+`;
 
 export default Contact
