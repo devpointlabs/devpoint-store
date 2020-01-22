53a0e0906ce3cddc65a34feeea9a655edb53ca6e
Faith <faithhafen@gmail.com>
2020-01-22 14:07:30 -0700

    merge

--- db/migrate/20200121214203_remove_column_from_item_variants.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromItemVariants < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :item_variants, :back_image, :string
-  end
-end
--- db/migrate/20200121214214_add_column_from_items.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnFromItems < ActiveRecord::Migration[6.0]
-  def change
-    add_column :items, :back_image, :string
-  end
-end
