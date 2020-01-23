6cdf6e02d6275bf89a82bd614807b6439443b098
David Mendez <mendezdavid68@gmail.com>
2020-01-23 12:21:37 -0700

    update

--- client/src/components/ItemVariantForm.js
+++ client/src/components/ItemVariantForm.js
@@ -43,6 +43,13 @@ class ItemVariantForm extends React.Component {
     }
   }
 
+  ItemOptions = [
+    { key: 1, text: 'TShirts', value: 'TShirts', id: 1 },
+    { key: 2, text: 'Hoodies', value: 'Hoodies', id: 3 },
+    { key: 3, text: 'Hats', value: 'Hats', id: 2 },
+    { key: 4, text: 'Stickers', value: 'Stickers', id: 4 },
+  ]
+
   render() {
     const { color, image, quantity, size } = this.state
     return (
