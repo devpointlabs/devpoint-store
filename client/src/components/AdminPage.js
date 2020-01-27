389dbfb990115350db55a4923980aa354ab12623
Faith <faithhafen@gmail.com>
2020-01-27 11:34:53 -0700

    added containers for admin and contact pages

--- client/src/components/AdminPage.js
+++ client/src/components/AdminPage.js
@@ -3,6 +3,7 @@ import CategoryForm from './CategoryForm'
 import ItemForm from './ItemForm'
 import ItemVariantForm from './ItemVariantForm'
 import BraintreeDrop from './BraintreeDrop'
+import { Container } from 'semantic-ui-react'
 
 class AdminPage extends React.Component {
   state = { categories: [], items: [], item_variant: [] }
@@ -10,12 +11,14 @@ class AdminPage extends React.Component {
   render() {
     return (
       <>
+      <Container>
         <CategoryForm />
           <br />
         <ItemForm category_id = {this.state.categories} />
           <br />
         <ItemVariantForm item_id = {this.state.items}/>
          <br />
+         </Container>
       </>
     )
   }
